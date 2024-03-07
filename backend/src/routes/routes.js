8a20fc55ead69cba55664bfb96ec3124c2d4cb88
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 12:11:46 -0300

    Add POST route for creating a client

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controllers = require('../controllers');
 
 router.get('/', controllers.getClients);
+router.post('/', controllers.createClient);
 
 module.exports = router;
