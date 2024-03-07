bf14bd133ed1426bca05d139c8813613789f65f9
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 12:09:07 -0300

    Add app.js file with database connection and routes

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const client = require('./database/connection');
+const router = require('./routes/routes');
+
+app.use(express.json());
+
+client.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+    return;
+  }
+  console.log('Conexão bem-sucedida com o banco de dados');
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+});
+
+app.use('/', router);
+
