c898b85b35d3f1771312adc7ea95183c43ad2e41
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 12:11:39 -0300

    Add createClient function to clients service

--- backend/src/services/clients.js
+++ backend/src/services/clients.js
@@ -10,6 +10,27 @@ const getClients = async () => {
   }
 };
 
+const createClient = async (
+  name,
+  email,
+  cpf,
+  phone,
+  coordinate_x,
+  coordinate_y
+) => {
+  try {
+    const result = await client.query(
+      'INSERT INTO clientes (nome, email, cpf, telefone, coordenada_x, coordenada_y) VALUES ($1, $2, $3, $4, $5, $6)',
+      [name, email, cpf, phone, coordinate_x, coordinate_y]
+    );
+    return result;
+  } catch (err) {
+    console.error('Erro ao inserir no banco de dados', err);
+    return { error: 'Erro ao inserir no banco de dados' };
+  }
+};
+
 module.exports = {
   getClients,
+  createClient,
 };
