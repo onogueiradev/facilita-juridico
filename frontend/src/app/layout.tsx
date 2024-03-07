cc1deac23366e6a18ccafa55144dea5890801c83
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 12:13:51 -0300

    Add Next.js configuration files and dependencies

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Clientes do serviço de limpeza",
+  description: "Clientes do serviço de limpeza",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
