287b5e4d3a85ec2d428b3516b7ffc4136c2412fd
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 16:41:55 -0300

    Add client dashboard and create client modal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,30 @@
-import Image from "next/image";
+'use client';
+import { useEffect } from "react";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import { CreateClientModal } from "@/components/FormsCreateClient/FormsCreateClient";
+
+import { useStore } from "@/store/store";
+import { getApiData } from "@/services";
 
 export default function Home() {
+
+  const { setRowsInitial, openDialog, setOpenDialog } = useStore();
+
+  useEffect(() => {
+    const request = async () => {
+      const data = await getApiData();
+      setRowsInitial(data);
+    }
+    request();
+  }, [setRowsInitial]);
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  }
   return (
-    <main>Hello World</main>
+    <main className="bg-gray-100 flex items-start h-screen w-[100vw] justify-start py-12">
+      <Dashboard />
+      <CreateClientModal open={openDialog} onClose={handleClose} />
+    </main>
   );
 }
