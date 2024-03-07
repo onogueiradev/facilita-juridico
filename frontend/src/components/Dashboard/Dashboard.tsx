f1108ae0213b349b048dbb00844443df35aa7cf9
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-07 16:41:25 -0300

    Add Dashboard component

---
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TableSorted } from '../Table/TableSorted'
+
+export default function Dashboard() {
+  return (
+    <section className='w-full h-3/4'>
+      <TableSorted />
+    </section>
+  )
+}
