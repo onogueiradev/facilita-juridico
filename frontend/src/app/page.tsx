27729ec54bbb27fc7aa61446b227feb46f8e9d0e
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-08 16:25:53 -0300

    Fix import statements and add loading indicator

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from "react";
 
-import Dashboard from "@/components/Dashboard/Dashboard";
+import { Dashboard } from "@/components/Dashboard/Dashboard";
 import { FormsClient } from "@/components/FormsClient/FormsClient";
 
 import { useStore } from "@/store/store";
@@ -9,15 +9,18 @@ import { fetchData } from "@/services";
 
 export default function Home() {
 
-  const { setRowsInitial, openDialog, setOpenDialog } = useStore();
+  const { setRowsInitial, openDialog, setOpenDialog, setIsLoading } = useStore();
 
   useEffect(() => {
     const request = async () => {
       const data = await fetchData();
       setRowsInitial(data);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000)
     }
     request();
-  }, [setRowsInitial]);
+  }, [setRowsInitial, setIsLoading]);
 
   const handleClose = () => {
     setOpenDialog(false);
