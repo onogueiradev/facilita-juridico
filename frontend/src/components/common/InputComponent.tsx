d30647d58d8fc1c80e02ecd49fb8570b28415120
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-08 14:57:14 -0300

    Update InputComponent props and add autocomplete attributes

--- frontend/src/components/common/InputComponent.tsx
+++ frontend/src/components/common/InputComponent.tsx
@@ -3,17 +3,26 @@ import { Grid, InputLabel, TextField } from '@mui/material';
 
 type Props = {
   label: string;
-  value: string;
+  value: string | number | undefined;
   onChange: (value: string) => void;
   placeholder?: string;
+  sm?: number;
+  type?: string;
 }
 
-export function InputComponent({ label, value, onChange, placeholder }: Props) {
+export function InputComponent({ label, value, onChange, placeholder, sm = 4, type = 'text' }: Props) {
   return (
-    <Grid item xs={12} sm={6}>
+    <Grid item xs={12} sm={sm}>
       <InputLabel>{label}</InputLabel>
       <TextField
-        type="text"
+        size='small'
+        type={type}
+        inputProps={{
+          autocomplete: 'new-password',
+          form: {
+            autocomplete: 'off',
+          },
+        }}
         value={value}
         onChange={(e) => onChange(e.target.value)}
         fullWidth
