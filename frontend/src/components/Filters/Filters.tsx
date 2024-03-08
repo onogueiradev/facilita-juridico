21f15c9d3d482cff5138f0621b8a1f450bc38963
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-08 14:57:56 -0300

    Refactor Filters component and add new functionality

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,21 +1,23 @@
 'use client';
-import React, { useState } from 'react';
-import { Typography, Grid } from '@mui/material';
-import { Close } from '@mui/icons-material';
+import React, { useCallback, useState } from 'react';
+import { Grid } from '@mui/material';
 
 import { ButtonComponent, CoordinateSelect, InputComponent } from '../common';
+import { MapComponent } from '../MapComponent/MapComponent';
 
 import { useStore } from '@/store/store';
 
 export function Filters() {
-  const { rowsInitial, setNewRows, setRowsInitial, setOpenDialog } = useStore();
+  const { rowsInitial, setNewRows, setRowsInitial, setOpenDialog, setActionError, setMessageAlertClient } = useStore();
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [cordinateX, setCordinateX] = useState('');
   const [cordinateY, setCordinateY] = useState('');
-  const [noResults, setNoResults] = useState(false);
+
+  const [openModal, setOpenModal] = useState(false);
+
 
   const cordinatesX = Array.from(new Set(rowsInitial.map((row) => row.coordenada_x).sort((a, b) => a - b)));
   const cordinatesY = Array.from(new Set(rowsInitial.map((row) => row.coordenada_y).sort((a, b) => a - b)));
@@ -24,8 +26,7 @@ export function Filters() {
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^a-zA-Z0-9 ]/g, '');
   }
 
-  const applyFilters = () => {
-    setNoResults(false);
+  const applyFilters = useCallback(() => {
     const filteredRows = rowsInitial?.filter((row) => {
       const formattedName = removeAccentsAndSpecialChars(name.toLowerCase());
       const formattedRowName = removeAccentsAndSpecialChars(row.nome.toLowerCase());
@@ -45,9 +46,16 @@ export function Filters() {
       return passName && passEmail && passPhone && passCordinateX && passCordinateY;
     });
 
-    setNoResults(filteredRows.length === 0);
+    setActionError(filteredRows.length === 0);
+    setMessageAlertClient('Nenhum resultado encontrado');
     setNewRows(filteredRows);
-  };
+
+    setTimeout(() => {
+      setActionError(false);
+      setMessageAlertClient('');
+    }, 5000);
+  }, [name, email, phone, cordinateX, cordinateY, rowsInitial, setNewRows, setActionError, setMessageAlertClient]);
+
 
   const resetFilters = () => {
     setName('');
@@ -57,49 +65,74 @@ export function Filters() {
     setCordinateY('');
     setRowsInitial(rowsInitial);
     setNewRows([])
-    setNoResults(false);
   };
 
+  const handleCreate = () => {
+    setOpenDialog(true)
+    resetFilters()
+  }
+  const handleVisitation = () => {
+    setOpenModal(true)
+    resetFilters()
+  }
+
+  const renderInputs = [
+    {
+      label: 'Nome',
+      value: name,
+      onChange: setName,
+      placeholder: 'Filtre por nome',
+
+    },
+    {
+      label: 'Email',
+      value: email,
+      onChange: setEmail,
+      placeholder: 'Filtre por email',
+
+    },
+    {
+      label: 'Telefone',
+      value: phone,
+      onChange: setPhone,
+      placeholder: 'Filtre por telefone',
+
+    },
+    {
+      label: 'Cordenada X',
+      value: cordinateX,
+      onChange: setCordinateX,
+      options: cordinatesX,
+    },
+    {
+      label: 'Cordenada Y',
+      value: cordinateY,
+      onChange: setCordinateY,
+      options: cordinatesY,
+    },
+  ]
   return (
     <Grid container spacing={2} className='p-6'>
-      <InputComponent label='Nome' value={name} onChange={setName} />
-      <InputComponent label='Email' value={email} onChange={setEmail} />
-      <CoordinateSelect
-        label='Cordenada X'
-        value={cordinateX}
-        onChange={setCordinateX}
-        options={cordinatesX}
-      />
-      <CoordinateSelect
-        label='Cordenada Y'
-        value={cordinateY}
-        onChange={setCordinateY}
-        options={cordinatesY}
-      />
-      <InputComponent
-        label='Telefone'
-        value={phone}
-        onChange={setPhone}
-      />
-      <Grid item xs={12} sm={6} className="flex items-center justify-end space-x-4">
+      {renderInputs.map((input, index) => {
+        return input.options
+          ? <CoordinateSelect key={index} {...input} />
+          : <InputComponent key={index} {...input} />
+      })}
+      <Grid item xs={12} sm={12} className="flex items-center justify-center space-x-4 mt-2">
         <ButtonComponent onClick={applyFilters} className=' bg-indigo-500 hover:bg-indigo-700 text-white'>
           Filtrar
         </ButtonComponent>
         <ButtonComponent onClick={resetFilters} className='bg-orange-500 hover:bg-orange-700 text-white'>
           Limpar
         </ButtonComponent>
-        <ButtonComponent onClick={() => setOpenDialog(true)} className=' bg-green-500 hover:bg-green-700 text-white'>
+        <ButtonComponent onClick={handleCreate} className=' bg-green-500 hover:bg-green-700 text-white'>
           Criar
         </ButtonComponent>
+        <ButtonComponent onClick={handleVisitation} className=' bg-purple-500 hover:bg-purple-700 text-white'>
+          Visitação
+        </ButtonComponent>
       </Grid>
-      {noResults && (
-        <Grid item xs={12} className="flex items-center justify-center mt-4">
-          <Typography variant="h6" className="text-red-500 bg-red-100 px-4 py-2 rounded-full flex items-center text-base">
-            <Close className="text-red-500" />
-            Nenhum resultado encontrado
-          </Typography>
-        </Grid>
-      )}
+      <MapComponent openModal={openModal} setOpenModal={setOpenModal} />
     </Grid>
   );
 }
