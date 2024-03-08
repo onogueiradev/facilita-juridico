0efec460c3907f5ffbc6ab7984bca40681bef44c
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-08 06:47:24 -0300

    Add visitation order route and update client route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,5 +3,7 @@ const controllers = require('../controllers');
 
 router.get('/', controllers.getClients);
 router.post('/', controllers.createClient);
+router.get('/visit-order', controllers.visitationOrder);
+router.put('/', controllers.updateClient);
 
 module.exports = router;
