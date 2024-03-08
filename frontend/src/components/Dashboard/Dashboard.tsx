f4cd29bfc5b86dbc4e492c99383bdec44c804f6d
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-08 14:58:10 -0300

    Update Dashboard section height

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+
 import { TableSorted } from '../Table/TableSorted'
 
 export default function Dashboard() {
   return (
-    <section className='w-full h-3/4'>
+    <section className='w-full h-full'>
       <TableSorted />
     </section>
   )
