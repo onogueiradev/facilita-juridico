68aa372a5caeb25076fb2da6587c1c319a276ec7
Vitor Nogueira <vslnogueira96@gmail.com>
2024-03-09 16:35:27 -0300

    Add messages.js file with error and success messages

---
+++ backend/src/utils/messages.js
@@ -0,0 +1,15 @@
+const messages = {
+  DATABASE_ERROR: 'Erro ao consultar o banco de dados',
+  INSERT_ERROR: 'Erro ao inserir no banco de dados',
+  UPDATE_ERROR: 'Erro ao atualizar no banco de dados',
+  DELETE_ERROR: 'Erro ao deletar no banco de dados',
+  DUPLICATE_EMAIL_ERROR:
+    'Email já cadastrado. Por favor, insira um email diferente e tente novamente.',
+  CLIENT_NOT_FOUND_ERROR: 'Cliente não encontrado',
+  ID_NUMBER_ERROR: 'ID precisa ser um número',
+  CLIENT_CREATED_SUCCESS: 'Cliente cadastrado com sucesso',
+  CLIENT_UPDATED_SUCCESS: 'Cliente atualizado com sucesso',
+  CLIENT_DELETED_SUCCESS: 'Cliente deletado com sucesso',
+  NO_CLIENTS_MESSAGE: 'Nenhum cliente cadastrado',
+};
+module.exports = messages;
